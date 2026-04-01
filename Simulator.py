82e8525a42e95efd5bf28e5cfaac50a0ca0e44e8
arshsaroha539-hue <arshsaroha539@gmail.com>
2026-04-01 14:49:58 +0530

    added changes in init, ALU decoder, ALU and immediate extend

--- Simulator.py
+++ Simulator.py
@@ -3,33 +3,16 @@ program_memory = {x:0 for x in range(0x0,0xFF+1,4)}
 stack_memory = {x:0 for x in range(0x100,0x17F + 1,4)}
 data_memory = {x:0 for x in range(0x10000,0x1007F+1,4)}
 register_file = {x: 0 for x in range(0,32)}
-register_file[2] = 0x0000017C #stack pointer init
+register_file[2] = 0x0000017C
 opcode_control_dict= {
-    #R-type(add, sub, and, or, etc.)
     "0110011": ("1", "_", "0", "0", "00", "0", "10", "0"),
-
-    #I-type(addi, sltui)
     "0010011": ("1", "000", "1", "0", "00", "0", "10", "0"),
-
-    #Load(lw)
     "0000011": ("1", "000", "1", "0", "01", "0", "00", "0"),
-
-    #Store(sw)
     "0100011": ("0", "001", "1", "1", "_", "0", "00", "0"),
-
-    #Branch(beq, bne, blt, bge, bltu, bgeu)
     "1100011": ("0", "011", "0", "0", "_", "1", "_", "0"),
-
-    #JAL
     "1101111": ("1", "010", "_", "0", "10", "0", "_", "1"),
-
-    #JALR
     "1100111": ("1", "000", "1", "0", "10", "0", "00", "1"),
-
-    #LUI
     "0110111": ("1", "100", "_", "0", "11", "0", "_", "0"),
-
-    #AUIPC
     "0010111": ("1", "100", "1", "0", "11", "0", "00", "0"),
 }
 class control_signal:
@@ -48,7 +31,6 @@ class control_signal:
         self.gte = None
         self.PCSRC = None
     def PCSRC_gen(self,instruction):
-        flag = None
         self.PCSRC = bool(self.JUMP)
         if instruction.funct3 == "000":
             self.PCSRC = bool((self.BRANCH*self.zero) + self.JUMP)
@@ -63,7 +45,7 @@ class control_signal:
         elif instruction.funct3 == "111":
             self.PCSRC = bool((self.gteu*self.BRANCH)+self.JUMP)
 class Instruction:
-    def __init__(self,binary_instruction,address): #instruction class will itself carry all of its results, so that calling a function only requires instruction as an attribute. Since fetch,decode,execute,memory,writeback will be called for every instruction, these attributes will eventually be assigned a value
+    def __init__(self,binary_instruction,address):
         self.address = address
         self.opcode = binary_instruction[25:]
         self.funct3 = binary_instruction[17:20]
@@ -71,44 +53,87 @@ class Instruction:
         self.rs1 = binary_instruction[12:17]
         self.rs2 = binary_instruction[7:12]
         self.rd = binary_instruction[20:25]
-        self.imm_ex = binary_instruction[0:24] #input to immediate extender
+        self.imm_ex = binary_instruction[0:24]
+        self._full_binary = binary_instruction
         self.control_signals = control_signal(self,address)
         self.ALURESULT = None
         self.PCplustarget = None
         self.PCplusfour = None
         self.read_data = None
-    def immediate_extend(self): #Use IMMSRC to ascertain how immediate is to be generated. IN the case of R-Type, simply return the integer zero
-        {}
+    def immediate_extend(self):
+        b = self.imm_ex
+        immsrc = self.control_signals.IMMSRC
+        if immsrc == "000":
+            bits = b[0:12]
+            value = 0
+            for i in range(12):
+                value = value * 2 + int(bits[i])
+            if bits[0] == "1":
+                value = value - 2**12
+            return value
+        if immsrc == "001":
+            bits = b[0:7] + b[20:24] + "0"
+            value = 0
+            for i in range(12):
+                value = value * 2 + int(bits[i])
+            if b[0] == "1":
+                value = value - 2**12
+            return value
+        if immsrc == "011":
+            bits = b[0] + self._full_binary[24] + b[1:7] + b[20:24] + "0"
+            value = 0
+            for i in range(13):
+                value = value * 2 + int(bits[i])
+            if b[0] == "1":
+                value = value - 2**13
+            return value
+        if immsrc == "010":
+            bits = b[0] + b[12:20] + b[11] + b[1:11] + "0"
+            value = 0
+            for i in range(21):
+                value = value * 2 + int(bits[i])
+            if b[0] == "1":
+                value = value - 2**21
+            return value
+        if immsrc == "100":
+            value = 0
+            for i in range(20):
+                value = value * 2 + int(b[i])
+            value = value * 2**12
+            return value
+        return 0
     def ALU_decoder(self):
-        op5f75 = (self.opcode[2]+ self.funct7[2])
+        op5 = self.opcode[2]
+        f75 = self.funct7[1]
         if self.control_signals.ALUOP == "00":
-            return("ADD")
-        elif self.control_signals.ALUOP == "01":
-            return("SUB")
-        elif self.control_signals.ALUOP == "10":
+            return "ADD"
+        if self.control_signals.ALUOP == "01":
+            return "SUB"
+        if self.control_signals.ALUOP == "10":
+            if self.funct3 == "000":
+                if op5 == "1" and f75 == "1":
+                    return "SUB"
+                else:
+                    return "ADD"
+            if self.funct3 == "001":
+                return "SLL"
             if self.funct3 == "010":
-                return("SLT")
-            elif self.funct3 == "110":
-                return("OR")
-            elif self.funct3 == "111":
-                return("AND")
-            elif self.funct3 == "001":
-                return("SLL")
-            elif self.funct3 == "011":
-                return("SLTU")
-            elif self.funct3 == "100":
-                return("XOR")
-            elif self.funct3 == "101":
-                return("SRL")
-            elif self.funct3 == "000" and (op5f75 == "00" or op5f75 == "01" or op5f75=="10"):
-                return("ADD")
-            elif self.funct3 == "000" and (op5f75 == "11"):
-                return("SUB")
-        elif self.control_signals.ALUOP=="_":
-            return("DO NOTHING")
-        else:
-            raise ValueError
-    def ADD(self,a,b): #a-rs1,b-rs2 
+                return "SLT"
+            if self.funct3 == "011":
+                return "SLTU"
+            if self.funct3 == "100":
+                return "XOR"
+            if self.funct3 == "101":
+                return "SRL"
+            if self.funct3 == "110":
+                return "OR"
+            if self.funct3 == "111":
+                return "AND"
+        if self.control_signals.ALUOP == "11":
+            return "ADD"
+        if self.control_signals.ALUOP == "_":
+            return "ADD"
+    def ADD(self,a,b):
         return(a+b)
     def SUB(self,a,b):
         return(a-b)
@@ -133,11 +158,52 @@ class Instruction:
         else:
             return(0)
     operations = {"ADD":ADD,"SUB":SUB,"XOR":XOR,"SRL":SRL,"SLL":SLL,"SLTU":SLTU,"OR":OR,"AND":AND,"SLT":SLT}
-    def ALU(self): #ALU function has to do the following- generate the zero,gteu,gte flags for every instruction,use ALU decoder to find the operation, and its sources, and store the result as the attribute ALURESULT
+    def ALU(self):
+        rs1_idx = int(self.rs1, 2)
+        A = register_file[rs1_idx]
+        if self.control_signals.ALUSRC == "1":
+            B = self.immediate_extend()
+        else:
+            rs2_idx = int(self.rs2, 2)
+            B = register_file[rs2_idx]
+        if self.opcode == "0110111":
+            self.ALURESULT = B % (2**32)
+            self.control_signals.zero = False
+            self.control_signals.gte = True
+            self.control_signals.gteu = True
+            return
+        if self.opcode == "0010111":
+            self.ALURESULT = (self.address + B) % (2**32)
+            self.control_signals.zero = False
+            self.control_signals.gte = True
+            self.control_signals.gteu = True
+            return
+        operation = self.ALU_decoder()
+        result = self.operations[operation](self, A, B)
+        self.ALURESULT = result % (2**32)
+        if A >= 2**31:
+            A_signed = A - 2**32
+        else:
+            A_signed = A
+        if B >= 2**31:
+            B_signed = B - 2**32
+        else:
+            B_signed = B
+        if A_signed == B_signed:
+            self.control_signals.zero = True
+        else:
+            self.control_signals.zero = False
+        if A_signed >= B_signed:
+            self.control_signals.gte = True
+        else:
+            self.control_signals.gte = False
+        if A >= B:
+            self.control_signals.gteu = True
+        else:
+            self.control_signals.gteu = False
+    def Memory_read_write(self):
         {}
-    def Memory_read_write(self):#Use the MEMWRITE attribute to ascertain whether data is to be written and load data from memory address(data_memory and stack_memory) into read_data attribute of instruction(WARNING: ensure addresses are positive and within bounds)
-        {} 
-    def register_write_back(self): #Write to register file if REGWRITE = "1"
+    def register_write_back(self):
         {}
 def fetch(binary_string,instruction_num):
     inst = Instruction(binary_string,instruction_num*(4))
